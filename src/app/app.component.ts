c5e5d1cff4f963d7242f1fdb19ac8ed75eacba6a
afolabiopakunle <opakunleafolabi@gmail.com>
2022-01-13 10:26:10 +0100

    subscription overload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
 
+  ngOnInit() {
+    // const timer$ = interval(1000);
+
+    // timer$.subscribe(val => {
+    //   console.log(val)
+    // })
+
+    // setTimeout(() => {
+    //   timer$.unsubscribe()
+    // })
+  }
+
+   
 }
